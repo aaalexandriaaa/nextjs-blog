dfba3115c854b0d5bf478f9d7ef02a977edbdcdd
aaalexandriaaa <67809768+aaalexandriaaa@users.noreply.github.com>
2021-02-03 14:55:51 -0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,49 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hi, I'm Alexandria - <br />a process-oriented, problem solving software developer
+who considers efficiency in every part of life.<br /><br />With an eye for finding both failure points and elegant solutions, I bring a shrewd understanding of systems to my work. Leveraging my background in non-profit, customer-first fields, I lead with open-ended questions and a collaborative mindset in order to help organizations build user-friendly solutions to advance their mission. </p>
+        <p>
+          (This is a sample website build with{' '}
+          <a href="https://nextjs.org/learn">the official Next.js tutorial</a>.)
+        </p>
+      </section>
+    <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
